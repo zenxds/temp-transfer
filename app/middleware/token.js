bb422e2f7205a4ec22629134c6695243fa6aecd2
zenxds <zenxds@gmail.com>
2022-06-06 11:16:12 +0800

    feat: update

--- app/middleware/token.js
+++ app/middleware/token.js
@@ -5,10 +5,9 @@ module.exports = async function (ctx, next) {
   const token = ctx.query.token || ctx.request.body.token || ctx.get('x-transfer-token')
 
   if (!validateToken(token)) {
-    const { file } = ctx.request.files
-    const filename = file.newFilename
-    if (filename) {
-      await services.redis.client.publish('__keyevent@0__:del', filename)
+    const { file } = ctx.request.files || {}
+    if (file) {
+      await services.redis.client.publish('__keyevent@0__:del', file.newFilename)
     }
 
     ctx.throw(401, 'token is invalid')
